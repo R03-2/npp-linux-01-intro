8c6b91659fa80131972fbfb1c123776552530e19
R03-2 <70034998+R03-2@users.noreply.github.com>
2025-02-18 22:21:44 -0800

    Update do-lab.sh

--- lab1/part1/do-lab.sh
+++ lab1/part1/do-lab.sh
@@ -4,34 +4,34 @@
 # This file will get called from capture_submission.sh
 
 
-docker exec -it clab-lab1-part1-switch ip link add name mybridge type bridge
-docker exec -it clab-lab1-part1-switch ip link set mybridge up
-docker exec -it clab-lab1-part1-switch ip link set eth1 master mybridge
-docker exec -it clab-lab1-part1-switch ip link set eth2 master mybridge
+docker exec -it clab-lab1-part1-switch   ip link add name mybridge type bridge
+docker exec -it clab-lab1-part1-switch   ip link set mybridge up
+docker exec -it clab-lab1-part1-switch   ip link set eth1 master mybridge
+docker exec -it clab-lab1-part1-switch   ip link set eth2 master mybridge
 
 
-docker exec -it clab-lab1-part2-switch ip link add name mybridge1 type bridge
-docker exec -it clab-lab1-part2-switch ip link set mybridge1 up
-docker exec -it clab-lab1-part2-switch ip link set eth2 master mybridge1
-docker exec -it clab-lab1-part2-switch ip link set eth3 master mybridge1
+docker exec -it clab-lab1-part1-switch   ip link add name mybridge1 type bridge
+docker exec -it clab-lab1-part1-switch   ip link set mybridge1 up
+docker exec -it clab-lab1-part1-switch   ip link set eth2 master mybridge1
+docker exec -it clab-lab1-part1-switch   ip link set eth3 master mybridge1
 
 
-docker exec -it clab-lab1-part1-switch ip link add name mybridge2 type bridge
-docker exec -it clab-lab1-part1-switch ip link set mybridge2 up
-docker exec -it clab-lab1-part1-switch ip link set eth1 master mybridge2
-docker exec -it clab-lab1-part1-switch ip link set eth3 master mybridge2
+docker exec -it clab-lab1-part1-switch   ip link add name mybridge2 type bridge
+docker exec -it clab-lab1-part1-switch   ip link set mybridge2 up
+docker exec -it clab-lab1-part1-switch   ip link set eth1 master mybridge2
+docker exec -it clab-lab1-part1-switch   ip link set eth3 master mybridge2
 
-docker exec -it clab-lab1-part1-switch ip link add name mybridge4 type bridge
-docker exec -it clab-lab1-part1-switch ip link set mybridge4 up
-docker exec -it clab-lab1-part1-switch ip link set eth1 master mybridge4
-docker exec -it clab-lab1-part1-switch ip link set eth4 master mybridge4
+docker exec -it clab-lab1-part1-switch   ip link add name mybridge4 type bridge
+docker exec -it clab-lab1-part1-switch   ip link set mybridge4 up
+docker exec -it clab-lab1-part1-switch   ip link set eth1 master mybridge4
+docker exec -it clab-lab1-part1-switch   ip link set eth4 master mybridge4
 
-docker exec -it clab-lab1-part2-switch ip link add name mybridge3 type bridge
-docker exec -it clab-lab1-part2-switch ip link set mybridge3 up
-docker exec -it clab-lab1-part2-switch ip link set eth2 master mybridge3
-docker exec -it clab-lab1-part2-switch ip link set eth4 master mybridge3
+docker exec -it clab-lab1-part1-switch   ip link add name mybridge3 type bridge
+docker exec -it clab-lab1-part1-switch   ip link set mybridge3 up
+docker exec -it clab-lab1-part1-switch   ip link set eth2 master mybridge3
+docker exec -it clab-lab1-part1-switch   ip link set eth4 master mybridge3
 
-docker exec -it clab-lab1-part3-switch ip link add name mybridge5 type bridge
-docker exec -it clab-lab1-part3-switch ip link set mybridge5 up
-docker exec -it clab-lab1-part3-switch ip link set eth3 master mybridge5
-docker exec -it clab-lab1-part3-switch ip link set eth4 master mybridge5
+docker exec -it clab-lab1-part1-switch ip link add name mybridge5 type bridge
+docker exec -it clab-lab1-part1-switch ip link set mybridge5 up
+docker exec -it clab-lab1-part1-switch ip link set eth3 master mybridge5
+docker exec -it clab-lab1-part1-switch ip link set eth4 master mybridge5
